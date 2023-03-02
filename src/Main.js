6dd602a94cbdc2c18b867f076e198a44e3c60d4f
yumelog <felicita.amore.u@gmail.com>
2023-03-03 01:09:11 +0900

    update get data process

--- src/Main.js
+++ src/Main.js
@@ -4,10 +4,11 @@ import axios from 'axios'
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 
 function Main() {
-  const [prefectures, setPrefectures] = React.useState(null)
+  const [prefectures, setPrefectures] = React.useState([])
+  const [chartData, setChartData] = React.useState({ prefs: [], data: [] })
+  const baseURL = 'https://opendata.resas-portal.go.jp'
 
   React.useEffect(() => {
-    const baseURL = 'https://opendata.resas-portal.go.jp'
     axios
       .get(baseURL + '/api/v1/prefectures', {
         headers: {
@@ -24,12 +25,49 @@ function Main() {
       })
   }, [])
   if (!prefectures) return null
-  const data = [
-    { name: '1980', 東京: 200000, 神奈川: 250000 },
-    { name: '1990', 東京: 400000, 神奈川: 500000 },
-    { name: '2000', 東京: 450000, 神奈川: 560000 },
-    { name: '2010', 東京: 700000, 神奈川: 900000 },
-  ]
+  const handleChange = (data) => {
+    const target = prefectures.find((elm) => {
+      return elm?.prefCode === Number(data.target.id)
+    })
+    axios
+      .get(`${baseURL}/api/v1/population/composition/perYear`, {
+        headers: {
+          'X-API-KEY	': process.env.REACT_APP_API_KEY,
+        },
+        params: {
+          prefCode: target.prefCode,
+        },
+      })
+      .then((res) => {
+        const result = res?.data?.result?.data.find((elm) => elm?.label === '総人口')?.data || []
+        const update_data = [...chartData.data]
+        let update_prefs = [...chartData.prefs]
+
+        if (update_prefs.find((elm) => elm === target.prefName)) {
+          update_data.map((elm) => {
+            const num = result.find((res) => res?.year === elm.year)
+            elm[target.prefName] = num
+            return elm
+          })
+          update_prefs = update_prefs.filter((elm) => elm !== target.prefName)
+        } else {
+          for (let i of result) {
+            if (update_data.find((elm) => elm?.year === i.year)) {
+              const target_data = update_data.findIndex((elm) => elm?.year === i.year)
+              update_data[target_data][target.prefName] = i.value
+            } else {
+              update_data.push({ year: i.year, [target.prefName]: i.value })
+            }
+          }
+          update_prefs.push(target.prefName)
+        }
+        setChartData({ prefs: update_prefs, data: update_data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   return (
     <div>
       <header className="title">RESAS</header>
@@ -43,9 +81,10 @@ function Main() {
                   <p>
                     <label htmlFor={`checkbox-${state.prefName}`}>{state.prefName}</label>
                     <input
-                      id={`checkbox-${state.prefName}`}
-                      name={`checkbox-${state.prefName}`}
+                      id={state.prefCode}
+                      name={state.prefName}
                       type="checkbox"
+                      onChange={handleChange}
                     />
                   </p>
                 </li>
@@ -53,12 +92,18 @@ function Main() {
             })}
           </ul>
         </div>
-        <div className="chart">
-          <LineChart width={400} height={400} data={data}>
-            <Line type="monotone" dataKey="東京" stroke="#8884d8" />
-            <Line type="monotone" dataKey="神奈川" stroke="#322454" />
+        <div className="chart-container">
+          <LineChart
+            width={800}
+            height={800}
+            margin={{ right: 50, left: 50 }}
+            data={chartData.data}
+          >
+            {chartData.prefs.map((elm, index) => {
+              return <Line key={index} type="monotone" dataKey={elm} stroke="#8884d8" />
+            })}
             <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey="name" />
+            <XAxis dataKey="year" />
             <YAxis />
             <Tooltip />
             <Legend />
